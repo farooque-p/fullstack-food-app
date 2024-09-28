a83dd038aa4d94b22ee582cdfc79adfdfd3f89b6
Farooque Parvez <farooqueparvez@gmail.com>
2024-09-28 17:23:55 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
-//import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
